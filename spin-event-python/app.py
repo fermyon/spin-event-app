68ef5662bcf7cacbf28be09c96f6cb15c3f8fbbe
quyumkehinde <quyumkehinde@gmail.com>
2024-08-31 11:10:34 +0100

    fix: cleanup http response
    Signed-off-by: quyumkehinde <quyumkehinde@gmail.com>

--- spin-event-python/app.py
+++ spin-event-python/app.py
@@ -7,7 +7,6 @@ import cloudevents
 
 class IncomingHandler(IncomingHandler):
     def handle_request(self, request: Request) -> Response:
-        print("Request received")
         if request.method == "GET":
             event = cloudevents.http.CloudEvent.create({
                 "type": "com.spin-event-python.person",
@@ -15,27 +14,25 @@ class IncomingHandler(IncomingHandler):
             }, {"user": {"id": 12, "name": "John Doe", "age": 24}})
             data = cloudevents.conversion.to_binary(event)
             
-            # Change to the address of your event gateway service (kubectl get svc -A)
             gateway_address = gateway_url()
             print(f"Sending event to {gateway_address}")
             request = Request("POST", gateway_address, data[0], data[1])
             try:
                 response = send(request)
+                if response.status != 200:
+                    print("Failed to publish event, got response: ", response)
+                    return build_response(response.status, response.body)
+                return build_response(200, "{\"message\": \"Successfully sent event!\"}")
             except Exception as e:
                 print("Error sending event", e)
-                return Response(500, {"content-type": "application/json"}, bytes(e, "utf-8"))
-            return Response(200, {"content-type": "application/json"}, bytes("{\"message\": \"Successfully sent CloudEvent!\"}", "utf-8"))
+                return build_response(500, "{\"message\": \"Failed to send event!\"}")
             
         if request.method == "POST":
             event = cloudevents.http.from_http(request.headers, request.body)
             print(f"Received event: {str(event)}")
-            return Response(
-                200,
-                {"content-type": "application/json"},
-                bytes("{\"message\": \"Successfully received CloudEvent!\"}", "utf-8")
-            )
-
-        return Response(200, {"content-type": "application/json"}, bytes("Method not allowed", "utf-8"))
+            return build_response(200, "{\"message\": \"Successfully received event!\"}")
+        
+        return build_response(405, "{\"message\": \"Method not allowed!\"}")
 
 def source_url():
     app_svc = variables.get("spin_app_svc")
@@ -48,3 +45,6 @@ def gateway_url():
     namespace = variables.get("gateway_namespace")
     gateway = f"http://{gateway_name}-service.{namespace}.svc.cluster.local"
     return gateway
+
+def build_response(status, body):
+    return Response(status, {"content-type": "application/json"}, bytes(body, "utf-8"))
